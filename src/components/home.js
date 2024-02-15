db3b0cb09640f24c861d1acde0a4e85ae7737fc1
Michelle <michellew.yeoh@gmail.com>
2024-02-15 13:37:14 -0800

    changed home

--- src/components/home.js
+++ src/components/home.js
@@ -4,9 +4,13 @@ import '../App.css';
 export const Home = () => {
     return (
         <div>
-            <h1 className='font'>Michelle's Personal Website!</h1>
-            <p className='navy'>about me!</p>
-            <p>Hi! My name is Michelle.</p>
+            <h1 className='font'>MICHELLE YEOH</h1>
+            <div style={{paddingRight: '500px', paddingLeft: '500px'}}>
+                <p className='navy'>about me!</p>
+                <p>Hello, my name is Michelle. I am a first-year computer science major. Welcome to my personal website!</p>
+
+            </div>
+            
         </div>
     )
 }
