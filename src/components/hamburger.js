a51ea1c98888b4b56145da2b47a2ec8274aa2788
Michelle <michellew.yeoh@gmail.com>
2024-03-21 21:39:28 -0700

    new burger

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "../styles/header.css"
-import {MdOutlineMenu} from 'react-icons/md'
-import {MdClose} from 'react-icons/md';
+// import {MdOutlineMenu} from 'react-icons/md'
+// import {MdClose} from 'react-icons/md';
 import { useState } from "react";
 
 
 
 export default function Hamburger(){
     const [click, setclick] = useState(false);
-    const Hamburger = <MdOutlineMenu className="HamburgerMenu"
-            size="30px" color="grey" onClick={() => setclick(!click)}/>
-    const Close = <MdClose className="HamburgerMenu"
-            size="30px" color="grey" onClick={() => setclick(!click)} />
+    const Hamburger = <img src="https://cdn4.iconfinder.com/data/icons/ui-admin-side-menu-set-32-vol-1/40/Menu_Burder-512.png" alt="burger" width="30px" onClick={() => setclick(!click)}/>
+    // <MdOutlineMenu className="HamburgerMenu"
+            // size="30px" color="grey" onClick={() => setclick(!click)}/>
+    const Close = <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Transparent_X.png" alt="close" width="20px" onClick={() => setclick(!click)}/>
+    // <MdClose className="HamburgerMenu"
+    //         size="30px" color="grey" onClick={() => setclick(!click)} />
     return (
         <hamburger>
             <a href="/" className='link'>ੈ✩‧₊˚michelle's page</a>  
